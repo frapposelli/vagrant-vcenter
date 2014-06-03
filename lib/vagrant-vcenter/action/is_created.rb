6e8d064c2072bc7d582748dad2321779dd52c5f9
Karl Pietri <barnaclebob@gmail.com>
2014-06-03 14:36:08 -0700

    make sure we are connected during the first test if the vm is created during up action.  also only test if the vm actually exists if we are connected to handle the rest of the actions

--- lib/vagrant-vcenter/action/is_created.rb
+++ lib/vagrant-vcenter/action/is_created.rb
@@ -12,15 +12,16 @@ module VagrantPlugins
           vm_id = env[:machine].id
           config = env[:machine].provider_config
           if vm_id
-            dc = config.vcenter_cnx.serviceInstance.find_datacenter(
-                config.datacenter_name) or abort 'datacenter not found'
-            root_vm_folder = dc.vmFolder
-            vm = root_vm_folder.findByUuid(env[:machine].id)
-            if not vm
-              @logger.warn('VM has not been created')
-              env[:result] = false
+            if config.vcenter_cnx
+              dc = config.vcenter_cnx.serviceInstance.find_datacenter(
+                  config.datacenter_name) or abort 'datacenter not found'
+              root_vm_folder = dc.vmFolder
+              vm = root_vm_folder.findByUuid(env[:machine].id)
+              if not vm
+                @logger.warn('VM has not been created')
+                env[:result] = false
+              end
             end
-              
             @logger.info("VM has been created and ID is: [#{vm_id}]")
             env[:result] = true
           else
